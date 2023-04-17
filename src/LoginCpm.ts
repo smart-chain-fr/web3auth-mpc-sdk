e4a09e259e4b05dc7f1d7957fb92942ce1e28cfe
Kévin Sanmartin <kevin.sanmartin@smart-chain.fr>
2023-04-17 16:07:11 +0200

    :art: naming

--- src/LoginCpm.ts
+++
@@ -1,9 +0,0 @@
-class LoginCpm extends HTMLElement {
-  // connect component
-  connectedCallback() {
-    this.textContent = "Hello World 2!";
-  }
-}
-
-customElements.define( 'w3c-login-view', LoginCpm );
-console.log("Hello 200");
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-import "./LoginCpm";
+class Login extends HTMLElement {
+  shadow: ShadowRoot;
 
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "closed" });
+  }
 
-console.log(LoginCpm);
+  // connect component
+  connectedCallback() {
+    this.shadow.innerHTML = `
+        <style></style>
+        <div class="popup">
+            <div class="popup-content">Bonjour</div>
+        </div>
+    `;
+  }
+}
+
+customElements.define("w3ac-login-view", Login);
