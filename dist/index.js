7892a92cfb035e1b852b227ee1f31621cecae017
Kévin Sanmartin <kevin.sanmartin@smart-chain.fr>
2023-04-18 17:03:59 +0200

    :sparkles: add children handling

--- dist/index.js
+++ dist/index.js
@@ -4,5 +4,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("./jsx");
-const Popup_1 = __importDefault(require("./Popup"));
-customElements.define("w3ac-modal", Popup_1.default);
+const Modal_1 = __importDefault(require("./Modal"));
+customElements.define("w3ac-modal", Modal_1.default);
